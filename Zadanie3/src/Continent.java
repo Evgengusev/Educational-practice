7a7df334eec56252b1b4b1050e11cf4765705247
Evgengusev <104605199+Evgengusev@users.noreply.github.com>
2022-12-07 17:59:28 +0300

    Add files via upload

---
+++ Zadanie3/src/Continent.java
@@ -0,0 +1,9 @@
+public class Continent {
+    private String name;
+    public Continent(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
